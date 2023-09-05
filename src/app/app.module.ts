a258237b6467d0031115c1fadafaa43d2a782570
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-09-05 22:52:37 +0300

    export header from shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './carts/components/cart/cart.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,5 +5,6 @@ import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [HeaderComponent],
   imports: [CommonModule],
+  exports: [HeaderComponent],
 })
 export class SharedModule {}
