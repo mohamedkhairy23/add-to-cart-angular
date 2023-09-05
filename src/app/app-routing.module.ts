f619bbf5644abe6f69e924755a76edea3806b49a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-09-06 00:27:47 +0300

    bootstrap installation and Prepare UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { DetailsComponent } from './products/components/details/details.component';
+import { CartComponent } from './carts/components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: AllProductsComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: 'products', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
